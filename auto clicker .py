4d6b520321b5bded72395dfe039ffa7d53ee1b4a
Dudu4k <97578092+Dudu4k@users.noreply.github.com>
2023-03-12 16:26:29 -0300

    Add files via upload

---
+++ auto clicker .py
@@ -0,0 +1,27 @@
+import pyautogui as pg
+import time
+from pynput import keyboard
+
+
+pg.FAILSAFE = True
+
+def stop_loop(key):
+    if key == keyboard.Key.esc:
+        return False  
+
+time.sleep(5)
+
+with keyboard.Listener(on_press=stop_loop) as listener:
+
+    while True:
+        
+        num_clicks = 20
+        for i in range(num_clicks):
+            pg.click()
+            time.sleep(100) 
+            
+            if not listener.running:
+                break  
+
+        if not listener.running:
+            break
